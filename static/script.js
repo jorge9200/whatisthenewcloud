f40dbedbf28a8284c667a3d4b20e3c32d6c5618b
Jorge Fernandez <jorge92f@gmail.com>
2017-06-26 18:07:18 +0200

    develop

---
+++ static/script.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    
+	var translate = function(){
+
+		$('.what-is .first-answer .text').html('The new Cloud is all about working<br>&nbsp;&nbsp;&nbsp;<b>online</b> with all your programs and<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;files instead of <b>syncing</b> your files<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;across all your devices.');
+		$('.what-is .second-answer .text').html('You can work and collaborate<br>directly online without having to<br>download, install, sync or<br>configure anything.');
+	}
+
+	translate();
+
+
+
+});
